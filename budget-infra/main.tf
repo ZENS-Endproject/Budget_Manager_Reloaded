ebf3097b884605d14581b1391b1f001ae80270fd
senda1988 <zidi.senda@gmail.com>
2025-10-30 12:33:58 +0100

    igw + route_table

--- budget-infra/main.tf
+++ budget-infra/main.tf
@@ -69,6 +69,31 @@ resource "aws_subnet" "public_subnet" {
   tags = { Name = "public_subnet" }
 }
 
+resource "aws_internet_gateway" "aws_internet_gateway" {
+ vpc_id = aws_vpc.my_vpc.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.my_vpc.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
 
 output "public_ip" {
     description = " Public IP EC2 instance"
