241933f24e3a0e5e419a3a3b9b83db8e20f4b357
senda1988 <zidi.senda@gmail.com>
2025-10-31 12:00:49 +0100

    new deploy

--- budget-infra/backend.tf
+++ budget-infra/backend.tf
@@ -3,7 +3,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "tf-bud-relo-state-bucket"
+    bucket         = "tf-bud-relo-state-bucket_f"
     key            = "infra/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
