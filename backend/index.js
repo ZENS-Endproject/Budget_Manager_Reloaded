f54b1c4eaffb23cbf1fe99bd425b3195aea06bfc
NassiTech <190089082+NassiTech@users.noreply.github.com>
2025-11-02 23:28:19 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,9 @@ const pool = new Pool({
   database: process.env.DB_NAME, // Name deiner Datenbank
   password: process.env.DB_PASSWORD, // Dein Passwort
   port: process.env.DB_PORT, // Standardport für PostgreSQL
+  ssl: {
+    rejectUnauthorized: false
+  }	
 });
 
 const createTable = async () => {
