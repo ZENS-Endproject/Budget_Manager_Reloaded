11ea3b9fdb8f2e00e44deb0da9eda4ed413da7f3
EmmaTechstarter <emma.feck@tn.techstarter.de>
2025-11-10 14:02:26 +0100

    Dark Mode + Hamburger Menü

--- frontend/src/layout/MainContent.js
+++ frontend/src/layout/MainContent.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function MainContent({ children }) {
+export default function MainContent({ children, onClickMain }) {
   return (
-    <main className="app-content flex-1 bg-[#FEFEEF]">
+    <main
+      className="app-content flex-1 bg-[#FEFEEF]"
+      onClick={onClickMain}
+    >
       <div className="app-content__inner pl-10 pr-16 py-6 w-full">
         {children}
       </div>
